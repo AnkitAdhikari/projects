aae70e69383abd4fbf07e07cd82a3cb51db62cc9
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-03-02 00:35:56 +0545

    update content and add functionality in all 3 item

--- modalWindow/script.js
+++ modalWindow/script.js
@@ -1,23 +1,63 @@
-const openModelAll = document.querySelectorAll(".textbox");
+// selecting image tag
 const openModelHTML = document.querySelector(".html-img");
+const openModelCSS = document.querySelector(".css-img");
+const openModelJS = document.querySelector(".js-img");
+// selecting overlay
 const overlay = document.querySelector(".overlay");
+// selecting respective image textbox
 const textBox_HTML = document.querySelector(".html-info-textbox");
-const closeModel = document.querySelector(".close-btn");
+const textBox_CSS = document.querySelector(".css-info-textbox");
+const textBox_JS = document.querySelector(".js-info-textbox");
+// selecting all cross/close btn
+const closeModel = document.querySelectorAll(".close-btn");
+// selecting all language/image
+const openModel = document.querySelectorAll(".img");
+
+function showOverlay() {
+  overlay.classList.remove("hidden");
+}
+function hideOverlay() {
+  overlay.classList.add("hidden");
+}
 
 openModelHTML.addEventListener("click", () => {
-  overlay.classList.remove("hidden");
   textBox_HTML.classList.remove("hidden");
+  showOverlay();
+});
+openModelCSS.addEventListener("click", () => {
+  textBox_CSS.classList.remove("hidden");
+  showOverlay();
+});
+openModelJS.addEventListener("click", () => {
+  textBox_JS.classList.remove("hidden");
+  showOverlay();
 });
 
 function removeModuleWindow() {
-  overlay.classList.add("hidden");
-  textBox_HTML.classList.add("hidden");
+  if (!textBox_HTML.classList.contains("hidden"))
+    textBox_HTML.classList.add("hidden");
+  if (!textBox_CSS.classList.contains("hidden"))
+    textBox_CSS.classList.add("hidden");
+  if (!textBox_JS.classList.contains("hidden"))
+    textBox_JS.classList.add("hidden");
 }
 
-closeModel.addEventListener("click", removeModuleWindow);
+for (let i of closeModel) {
+  i.addEventListener("click", hideOverlay);
+  i.addEventListener("click", removeModuleWindow);
+}
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
-    removeModuleWindow();
+    if (!textBox_HTML.classList.contains("hidden")) {
+      textBox_HTML.classList.add("hidden");
+    }
+    if (!textBox_CSS.classList.contains("hidden")) {
+      textBox_CSS.classList.add("hidden");
+    }
+    if (!textBox_JS.classList.contains("hidden")) {
+      textBox_JS.classList.add("hidden");
+    }
+    hideOverlay();
   }
 });
