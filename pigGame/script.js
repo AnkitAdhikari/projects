b27b43810c77656ff38d9a7efed5dbbeee94e85e
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-03-03 21:42:01 +0545

    add gameover state

--- pigGame/script.js
+++ pigGame/script.js
@@ -11,6 +11,7 @@ const btnHold = document.querySelector(".btn-hold");
 const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let isGameOver = false;
 
 function switchPlayer() {
   currentScore = 0;
@@ -21,27 +22,32 @@ function switchPlayer() {
 }
 
 btnRoll.addEventListener("click", () => {
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // checking for 1
-  if (!(dice === 1)) {
-    // showing in dom
-    currentScore += dice;
-    console.log(dice);
-    document.querySelector(`.player--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    switchPlayer();
+  if (!isGameOver) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // checking for 1
+    if (!(dice === 1)) {
+      // showing in dom
+      currentScore += dice;
+      console.log(dice);
+      document.querySelector(`.player--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener("click", () => {
-  score[activePlayer] += currentScore;
-  document.querySelector(`.score--${activePlayer}`).textContent =
-    score[activePlayer];
-  //winning condition
-  if (score[activePlayer] >= 20) {
-    document.querySelector(`.player--${activePlayer}`).textContent = "winner";
-  } else {
-    switchPlayer(); //also make currscore = 0
+  if (!isGameOver) {
+    score[activePlayer] += currentScore;
+    document.querySelector(`.score--${activePlayer}`).textContent =
+      score[activePlayer];
+    //winning condition
+    if (score[activePlayer] >= 20) {
+      document.querySelector(`.player--${activePlayer}`).textContent = "winner";
+      isGameOver = true;
+    } else {
+      switchPlayer(); //also make currscore = 0
+    }
   }
 });
