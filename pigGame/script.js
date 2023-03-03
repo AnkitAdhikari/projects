e79d0890f2f96b7f4ea002c4665dade558edaf92
AnkitAdhikari <ankitadhikari31512@gmail.com>
2023-03-03 21:16:17 +0545

    add switch active player  feature when dice is 1

--- pigGame/script.js
+++ pigGame/script.js
@@ -12,8 +12,16 @@ let activePlayer = 0;
 
 btnRoll.addEventListener("click", () => {
   const dice = Math.trunc(Math.random() * 6) + 1;
-  currentScore += dice;
-  console.log(dice);
-  document.querySelector(`.player--${activePlayer}`).textContent = currentScore;
+  // checking for 1
+  if (!(dice === 1)) {
+    // showing in dom
+    currentScore += dice;
+    console.log(dice);
+    document.querySelector(`.player--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    section0.classList.toggle("active--player");
+    section1.classList.toggle("active--player");
+  }
 });
 console.log(btnRoll);
